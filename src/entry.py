a7b050fe3869b1315815d8aba9854f013ef1ce04
Hugo-C <Hugo-C@users.noreply.github.com>
2024-07-04 23:39:15 +0200

    fix missing await

--- src/entry.py
+++ src/entry.py
@@ -12,7 +12,7 @@ from worker import ThreatFoxJarmer
 async def on_fetch(request):
     start = time.time()
     jarmer = ThreatFoxJarmer()
-    processed = jarmer.compute_jarms_of_last_day_c2()
-    jarmer.shutdown()
+    processed = await jarmer.compute_jarms_of_last_day_c2()
+    await jarmer.shutdown()
     duration = time.time() - start
     return Response.new(f"Processed: {processed}, took {duration} seconds")
